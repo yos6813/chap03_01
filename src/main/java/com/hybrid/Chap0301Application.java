55d887b776e0d75cf724f3cd3f6db53883770305
java <java>
2016-07-21 17:43:32 +0900

    test

---
+++ src/main/java/com/hybrid/Chap0301Application.java
@@ -0,0 +1,12 @@
+package com.hybrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap0301Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap0301Application.class, args);
+	}
+}
---
+++ src/main/java/com/hybrid/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.hybrid;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Chap0301Application.class);
+	}
+
+}
